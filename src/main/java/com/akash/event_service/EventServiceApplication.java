324b110e3c0b5ec3e14c3c9443a083be89ab2df4
Akash Ayyadevara <ayyadevaraakash@gmail.com>
2025-11-08 13:18:46 +0530

    backend work with jwt

--- src/main/java/com/akash/event_service/EventServiceApplication.java
+++ src/main/java/com/akash/event_service/EventServiceApplication.java
@@ -1,19 +1,33 @@
 package com.akash.event_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.akash.event_service.auth.model.Role;
+import com.akash.event_service.auth.model.User;
+import com.akash.event_service.auth.repo.UserRepository;
+import com.akash.event_service.auth.service.JwtService;
 import com.akash.event_service.model.Event;
 
 @SpringBootApplication
-public class EventServiceApplication {
+public class EventServiceApplication implements CommandLineRunner {
+	@Autowired
+	private JwtService jwtService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventServiceApplication.class, args);
-		Event e = new Event();
-		e.setTitle("Demo");
-		System.out.println(e.getTitle());
-
+		SpringApplication.run(EventServiceApplication.class, args);		
 	}
+	
+
+
+	@Override
+	public void run(String... args) {
+	    String token = jwtService.generateToken("akash@gmail.com");
+	    System.out.println("Generated Token: " + token);
+	    System.out.println("Extracted: " + jwtService.extractUsername(token));
+	}
+
 
 }
